15c201ea0de5fbbaad6aacd7f7c44a9fbe7d9f1b
Sean Finley <finleysean9@gmail.com>
2023-12-14 16:56:13 -0500

    minor changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
