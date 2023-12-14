4fb9523aeb712e5db47e70e413a73a51947238aa
Sean Finley <finleysean9@gmail.com>
2023-12-14 00:35:57 -0500

    changed path name

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
-const router = require('express').Router();
+const express = require("express");
+const router = express.Router()
+
 const path = require('path');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const router = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,12 +13,11 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-app.use(routes);
+app.use(router);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
