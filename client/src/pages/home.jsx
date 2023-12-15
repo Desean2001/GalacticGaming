90c30d823c976ecd4892f61f45655bdf103789d8
Sean Finley <finleysean9@gmail.com>
2023-12-14 19:36:18 -0500

    css

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -4,12 +4,10 @@ import { Container, Jumbotron } from 'react-bootstrap';
 const HomePage = () => {
   return (
     <Container fluid>
-      <Jumbotron>
         <h1 className='text-light'>Welcome to GalacticGaming</h1>
         <p className='text-light'>
           Explore the universe of gaming adventures and beyond. May the force of great games be with you!
         </p>
-      </Jumbotron>
     </Container>
   );
 };
