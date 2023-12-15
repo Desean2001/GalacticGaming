a9386ada40f79747efaf86a5f9f4e0a67183143e
Sean Finley <finleysean9@gmail.com>
2023-12-14 20:23:02 -0500

    css

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -6,10 +6,10 @@ const HomePage = () => {
   return (
     <Container fluid>
         <div className='vertical-center-container'>
-            <h1 className='text-caution'>Welcome to Galactic Gaming</h1>
+            <h1 className='text-light'>Welcome to Galactic Gaming</h1>
         </div>
         <div className='vertical-center-container'>
-            <p className='text-caution'>
+            <p className='text-light'>
                 Explore the universe of gaming adventures and beyond. May the force of great games be with you!
             </p>
         </div>
